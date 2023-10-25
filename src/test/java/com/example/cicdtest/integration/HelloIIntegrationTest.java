2de17fdf8d645d29c2e6e201f8dafad219261605
rimrim990 <lim990718@naver.com>
2023-10-26 00:10:22 +0900

    test: 스프링 통합 테스트 작성

---
+++ src/test/java/com/example/cicdtest/integration/HelloIIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.example.cicdtest.integration;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("HelloController 통합 테스트")
+public class HelloIIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("요청을 보내면 hello 를 반환한다")
+    void returnHello() throws Exception {
+        mockMvc.perform(get("/hello"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("hello"));
+    }
+}
