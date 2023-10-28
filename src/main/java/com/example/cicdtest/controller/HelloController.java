a5fa47a6566f7e1cb05a6d77b5649170c081af5f
rimrim990 <lim990718@naver.com>
2023-10-29 01:53:03 +0900

    feat: delete API 추가

--- src/main/java/com/example/cicdtest/controller/HelloController.java
+++ src/main/java/com/example/cicdtest/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.cicdtest.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,9 @@ public class HelloController {
     @PostMapping("/hello")
     public String createHello() {
         return "hello";
+    }
+
+    @DeleteMapping("/hello")
+    public void deleteHello() {
     }
 }
